b449d94c19d0256101c2f274672e92b7cf7ebdc9
fidanakhundova <114071391+fidanakhundova@users.noreply.github.com>
2022-10-31 15:26:54 +0400

    Add files via upload

---
+++ hey/project.js
@@ -0,0 +1,9 @@
+const plus = document.querySelector('.plus')
+const add = document.querySelector('.add')
+const sort = document.querySelector('.sorting')
+
+plus.addEventListener('keyup', function(event){
+  if (event.key == 'Enter')
+  console.log(this.value)
+}
+)
